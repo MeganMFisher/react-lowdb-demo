2e3e03221582ff982f19ad645e0bf520227cccf4
MeganMFisher <meganmichelle318@gmail.com>
2017-08-12 08:03:20 -0600

    get working

--- server.js
+++ server.js
@@ -1,42 +1,52 @@
 const express = require('express')
 const low = require('lowdb')
-// const fileAsync = require('lowdb/lib/storages/file-async')
+const fileAsync = require('lowdb/lib/storages/file-async')
 
 // Create server
 const app = express()
 
 // Start database using file-async storage
 // For ease of use, read is synchronous
-// const db = low('db.json', {
-//   storage: fileAsync
-// })
+const db = low('db.json', {
+  storage: fileAsync
+})
+
 
 // Routes
-// GET /posts/:id
-// app.get('/posts/:id', (req, res) => {
-//   const post = db.get('posts')
+// GET /products
+app.get('/allProducts', (req, res) => {
+  const allProducts = db.get('allProducts')
+    .value()
+    // console.log(allProducts)
+  res.send(allProducts)
+})
+
+
+// GET /product/:id
+// app.get('/product/:id', (req, res) => {
+//   const product = db.get('allProducts')
 //     .find({ id: req.params.id })
 //     .value()
-
-//   res.send(post)
+// console.log(product)
+//   res.send(product)
 // })
 
 // POST /posts
-// app.post('/posts', (req, res) => {
-//   db.get('posts')
-//     .push(req.body)
-//     .last()
-//     .assign({ id: Date.now() })
-//     .write()
-//     .then(post => res.send(post))
-// })
+app.post('/allProducts', (req, res) => {
+    console.log(req.body)
+  const added = db.get('allProducts')
+    .push(req.body)
+    .last()
+    .assign({ id: 4 })
+    .write()
+    .then(post => res.send(post))
+})
+
+
 
 // Init
-// db.defaults({ posts: [] })
+// db.defaults({ allProducts: [{item: 'Sweater', id: 1}, {item: 'Jelly Shoes', id: 2 }, {item: 'WindBreaker', id: 3}] })
 //   .write()
 //   .then(() => {
-//     app.listen(3000, () => console.log('Server is listening')
-//   })
-
-
-app.listen(3004);
+    app.listen(4004, () => console.log('Server is listening'))
+// })
